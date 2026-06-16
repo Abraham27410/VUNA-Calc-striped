087837ab838746457c8cd40c4a9b9146c71da49a
Abraham27410 <your.abrahamsangari16@gmail.com>
2026-06-16 19:12:59 +0100

    Updated homepage

--- VUNA-Calc-striped-main/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  collectCoverageFrom: [
-    'calculator/assets/js/script.js',
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-};
